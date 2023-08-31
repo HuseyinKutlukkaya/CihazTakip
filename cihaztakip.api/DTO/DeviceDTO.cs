3bc4982aad3927900827a15999bbaddf11805b4f
HuseyinKutlukkaya <huseyinkutlukkaya@gmail.com>
2023-08-31 22:26:52 +0300

    Add DeviceController,DeviceDTO

---
+++ cihaztakip.api/DTO/DeviceDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace cihaztakip.api.DTO
+{
+    public class DeviceDTO
+    {
+        public int DeviceId { get; set; }
+        public string Name { get; set; }
+    }
+}
--- cihaztakip.api/Program.cs
+++ cihaztakip.api/Program.cs
@@ -1,3 +1,10 @@
+using cihaztakip.business.Concrete;
+using cihaztakip.business.Abstract;
+using cihaztakip.data.Abstract;
+using cihaztakip.data.Concrete.EfCore;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers(options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true);
+
+builder.Services.AddDbContext<ApplicationDbContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddScoped<IDeviceService, DeviceManager>();
 
 var app = builder.Build();
 
