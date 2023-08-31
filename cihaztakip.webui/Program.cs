df20dbd60a8758c767b922c0e61cf9613100d588
HuseyinKutlukkaya <huseyinkutlukkaya@gmail.com>
2023-08-31 21:27:25 +0300

    Add Connection to program.cs

--- cihaztakip.webui/Program.cs
+++ cihaztakip.webui/Program.cs
@@ -1,7 +1,11 @@
+using cihaztakip.data.Concrete.EfCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
