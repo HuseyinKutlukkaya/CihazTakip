9306501c2eb1c799005926746b35d3f96fc31ac4
HuseyinKutlukkaya <huseyinkutlukkaya@gmail.com>
2023-08-31 20:54:36 +0300

    Add Repository Interfaces

---
+++ cihaztakip.data/Abstract/IDeviceRepository.cs
@@ -0,0 +1,13 @@
+﻿using cihaztakip.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cihaztakip.data.Abstract
+{
+    public interface IDeviceRepository : IRepository<Device> 
+    {
+    }
+}
