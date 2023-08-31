6b08bbf1a1ad9fcdbdd95a4fbc0c98c1b882cab3
HuseyinKutlukkaya <huseyinkutlukkaya@gmail.com>
2023-08-31 20:12:38 +0300

    Add project files.

---
+++ cihaztakip.webui/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cihaztakip.webui.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
