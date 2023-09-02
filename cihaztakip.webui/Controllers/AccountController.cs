398facf8148bb5a9ab9ce52409325514a40cbb11
HuseyinKutlukkaya <huseyinkutlukkaya@gmail.com>
2023-09-02 17:17:46 +0300

    Add Login,Register functions

--- cihaztakip.webui/Controllers/AccountController.cs
+++ cihaztakip.webui/Controllers/AccountController.cs
@@ -1,12 +1,95 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using cihaztakip.entity;
+using cihaztakip.webui.Models;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace cihaztakip.webui.Controllers
 {
+    [AutoValidateAntiforgeryToken]
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        private UserManager<User> _userManager;
+        private SignInManager<User> _signInManager;
+
+        public AccountController( UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+
+        }
+        public IActionResult AccessDenied()
         {
             return View();
+        }
+        public IActionResult Login(string? ReturnUrl = null)
+        {
+            return View(new LoginModel()
+            {
+                ReturnUrl = ReturnUrl
+            });
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user == null)
+            {
+                ModelState.AddModelError("", "Bu kullanıcı adı ile bir kayıt yoktur");
+                return View(model);
+            }
+
+            //if(!await _userManager.IsEmailConfirmedAsync(user))
+            //{
+            //    ModelState.AddModelError("", "Mail ile hesabınızı onaylayın.");
+            //    return View(model);
+            //}
+            var result = await _signInManager.PasswordSignInAsync(user, model.Password, false, false);
+            if (result.Succeeded)
+            {
+                return Redirect(model.ReturnUrl ?? "~/");
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+
+                return View(model);
+            }
+            var user = new User()
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                UserName = model.UserName,
+                Email = model.Email,
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+
+                return RedirectToAction("Login", "Account");
+            }
+            ModelState.AddModelError("", "Bir Sıkıntı var tekrar dene.");
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return Redirect("~/");
         }
     }
 }
