d1e744b0327939241467913520f0c6c4332dde2a
HuseyinKutlukkaya <huseyinkutlukkaya@gmail.com>
2023-09-02 14:04:06 +0300

    Add AccountController, Identity options and configurations

---
+++ cihaztakip.webui/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cihaztakip.webui.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- cihaztakip.webui/Program.cs
+++ cihaztakip.webui/Program.cs
@@ -16,6 +16,39 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
+builder.Services.Configure<IdentityOptions>(options => {
+    // password
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = true;
+
+    // Lockout                
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.AllowedForNewUsers = true;
+
+   
+    options.User.RequireUniqueEmail = true;
+
+});
+
+builder.Services.ConfigureApplicationCookie(options => {
+    options.LoginPath = "/account/login";
+    options.LogoutPath = "/account/logout";
+    options.AccessDeniedPath = "/account/accessdenied";
+    options.SlidingExpiration = true;
+    options.ExpireTimeSpan = TimeSpan.FromDays(30);
+    options.Cookie = new CookieBuilder
+    {
+        HttpOnly = true,
+        Name = ".CihazTakip.Security.Cookie",
+        SameSite = SameSiteMode.Strict
+    };
+});
+
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IDeviceService, DeviceManager>();
 
