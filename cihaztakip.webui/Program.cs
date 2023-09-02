71c1c60c600af460a8313f7c984fcbd895d920a8
HuseyinKutlukkaya <huseyinkutlukkaya@gmail.com>
2023-09-02 12:30:15 +0300

    Add Services for identity,efcore to program.cs

--- cihaztakip.webui/Program.cs
+++ cihaztakip.webui/Program.cs
@@ -1,4 +1,10 @@
+using cihaztakip.business.Abstract;
+using cihaztakip.business.Concrete;
+using cihaztakip.data.Abstract;
 using cihaztakip.data.Concrete.EfCore;
+using cihaztakip.entity;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +12,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IDeviceService, DeviceManager>();
+
+
 
 var app = builder.Build();
 
