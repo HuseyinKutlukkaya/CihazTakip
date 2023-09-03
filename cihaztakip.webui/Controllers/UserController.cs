5383db06f31f86dabc6e9d9d0a5a3da71bc6af86
HuseyinKutlukkaya <huseyinkutlukkaya@gmail.com>
2023-09-03 14:23:54 +0300

    Add User edit function

--- cihaztakip.webui/Controllers/UserController.cs
+++ cihaztakip.webui/Controllers/UserController.cs
@@ -9,12 +9,12 @@ namespace cihaztakip.webui.Controllers
     {
         private UserManager<User> _userManager;
         private SignInManager<User> _signInManager;
-
-        public UserController(UserManager<User> userManager, SignInManager<User> signInManager)
+        private RoleManager<IdentityRole> _roleManager;
+        public UserController(UserManager<User> userManager, SignInManager<User> signInManager,RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
-
+            _roleManager = roleManager;
         }
         public IActionResult UserList()
         {
@@ -33,6 +33,62 @@ namespace cihaztakip.webui.Controllers
 
            
             return View(userdata);
+        }
+        public async Task<IActionResult> UserEdit(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                var selectedRoles = await _userManager.GetRolesAsync(user);
+                var roles = _roleManager.Roles.Select(role => role.Name).ToList();
+
+                ViewBag.Roles = roles;
+                return View(new UserDetailsModel()
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Email = user.Email,
+                    EmailConfirmed = user.EmailConfirmed,
+                    SelectedRoles = selectedRoles
+                });
+            }
+
+            return RedirectToAction("UserList"); // If there is no user with the selected id
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserEdit(UserDetailsModel model, string[] selectedRoles)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByIdAsync(model.UserId);
+                if (user != null)
+                {
+                    user.FirstName = model.FirstName;
+                    user.LastName = model.LastName;
+                    user.UserName = model.UserName;
+                    user.Email = model.Email;
+                    user.EmailConfirmed = model.EmailConfirmed;
+
+                    var result = await _userManager.UpdateAsync(user);
+
+                    if (result.Succeeded)
+                    {
+                        var userRoles = await _userManager.GetRolesAsync(user);
+                        selectedRoles = selectedRoles ?? new string[] { };
+                        await _userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles).ToArray<string>());
+                        await _userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles).ToArray<string>());
+
+                        return Redirect("UserList");
+                    }
+                }
+                return Redirect("UserList");
+            }
+
+            return View(model);
+
         }
     }
 }
